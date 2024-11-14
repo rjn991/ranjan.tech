72178a95030bccf881a8538424e42abee0b9b59d
rjn991 <ranjanht991@gmail.com>
2024-11-14 13:16:03 +0530

    added transparent dp

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
+    <>
     <div className="min-h-screen flex flex-col">
       <div>nav</div>
       <HeroHighlight>
@@ -24,19 +25,22 @@ function App() {
             duration: 0.5,
             ease: [0.4, 0.0, 0.2, 1],
           }}
-          className="flex gap-4 md:gap-20 items-center text-5xl font-bold text-neutral-700 dark:text-white leading-snug sm:leading-relaxed  mx-auto flex-col-reverse md:flex-row sm:flex-row "
+          className="flex gap-4 sm:gap-20 items-center text-5xl font-bold text-neutral-700 dark:text-white leading-snug sm:leading-relaxed  mx-auto flex-col-reverse md:flex-row sm:flex-row "
           >
           <div className="flex-[1]">
             <p> Hello,<br></br>I am Ranjan</p>
            
           </div>
           <div className="">
-            <img className=" block rounded-full max-h-[200px]" src={dp}></img>
+            <img className=" block rounded-full max-h-[250px] bg-black" src={dp}></img>
           </div>
         </motion.h1>
       </HeroHighlight>
-      <div>nav</div>
     </div>
+    <div>
+      Hello
+    </div>
+    </>
   );
 }
 
